5bc76c423e2a571e8df903f9f3af85d6dd2dc5fe
Boburmirzo <bobur.isokov@spitamen-it.com>
2020-05-17 16:15:34 -0700

    MegabetAppCreated

---
+++ TestProgect/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Webkit;
+
+namespace testProgect
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = false)]
+    public class MainActivity : AppCompatActivity
+    {
+        WebView webView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            webView = FindViewById<WebView>(Resource.Id.webView);
+            webView.SetWebViewClient(new ExtendWebViewClient());
+
+            WebSettings webSettings = webView.Settings;
+            webSettings.JavaScriptEnabled = true;
+
+            webView.LoadUrl("https://megabet24.com/ru/Sport");
+        }
+    }
+
+    internal class ExtendWebViewClient : WebViewClient
+    {
+        [System.Obsolete]
+        public override bool ShouldOverrideUrlLoading(WebView view, string url)
+        {
+            view.LoadUrl(url);
+            return true;
+        }
+    }
+}
---
+++ TestProgect/SplashScreen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Java.Util;
+
+namespace testProgect
+{
+    [Activity(Label = "SplashScreen", MainLauncher =true, Theme ="@style/Theme.AppCompat.Light.NoActionBar")]
+    public class SplashScreen : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            int progress = 0;
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SplashScreen);
+            var progressBar = FindViewById<Android.Widget.ProgressBar>(Resource.Id.progressBar1);
+            var TextViewer = FindViewById<Android.Widget.TextView>(Resource.Id.textView1);
+           new Thread(new ThreadStart(delegate 
+               {
+                   while (progress < 100)
+                   {
+                       progress += 1;
+                       progressBar.Progress = progress;
+                       TextViewer.Text = "Downloding "+Convert.ToString(progress)+"%";
+                       Thread.Sleep(50);
+                   }
+                   StartActivity(new Intent(this, typeof(MainActivity)));
+               })).Start();
+        }
+    }
+}
